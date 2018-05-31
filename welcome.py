08f69ee46089f0091199d1ab0bc90d60cf59264f
Alfa Yohannis <ary506@york.ac.uk>
2018-05-31 20:10:37 +0100

    Timer on Welcome Form
    Timer on Welcome Form

--- welcome.py
+++ welcome.py
@@ -1,5 +1,7 @@
 from PyQt4 import QtGui, uic
-from PyQt4.QtGui import QMessageBox
+from customutil import CountDownTimer
+
+COUNTDOWN_COUNT = 1
 
 
 class WelcomeForm(QtGui.QMainWindow):
@@ -9,6 +11,19 @@ class WelcomeForm(QtGui.QMainWindow):
         self.main_program = main_program
         uic.loadUi('ui/welcome.ui', self)
         self.button_next.clicked.connect(self.on_button_next_click)
+
+        self.counter = COUNTDOWN_COUNT
+        self.countDownTimer = CountDownTimer(10, 0, 1, self.countdown)
+        self.countDownTimer.start()
+
+    def countdown(self):
+        if self.counter > 0:
+            self.button_next.setText("Next (" + str(self.counter) + ")")
+            self.counter -= 1
+        else:
+            self.countDownTimer.stop()
+            self.button_next.setEnabled(True)
+            self.button_next.setText("Next")
 
     def on_button_next_click(self):
         self.main_program.show_next_form()
